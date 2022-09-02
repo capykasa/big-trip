82e73d032f14fe376c114c7d552e0b429b5464b9
capykasa <capykasa@gmail.com>
2022-09-02 19:32:04 +0500

    Подкорректировал сортировку. Не работает флаг checked

--- src/view/trip-sort-view.js
+++ src/view/trip-sort-view.js
@@ -1,10 +1,10 @@
 import { SortType } from '../const.js';
 import AbstractView from '../framework/view/abstract-view.js';
 
-const createTripSortTemplate = () => (
+const createTripSortTemplate = (currentSortType) => (
   `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
     <div class="trip-sort__item  trip-sort__item--day">
-      <input id="sort-day" class="trip-sort__input  visually-hidden" data-sort-type="${SortType.DAY}" type="radio" name="trip-sort" value="sort-day" checked>
+      <input id="sort-day" class="trip-sort__input  visually-hidden" data-sort-type="${SortType.DAY}" type="radio" name="trip-sort" value="sort-day" ${currentSortType === SortType.DAY ? 'checked' : ''}>
         <label class="trip-sort__btn" for="sort-day">Day</label>
     </div>
 
@@ -19,7 +19,7 @@ const createTripSortTemplate = () => (
     </div>
 
     <div class="trip-sort__item  trip-sort__item--price">
-      <input id="sort-price" class="trip-sort__input  visually-hidden" data-sort-type="${SortType.PRICE}" type="radio" name="trip-sort" value="sort-price">
+      <input id="sort-price" class="trip-sort__input  visually-hidden" data-sort-type="${SortType.PRICE}" type="radio" name="trip-sort" value="sort-price" ${currentSortType === SortType.PRICE ? 'checked' : ''}>
         <label class="trip-sort__btn" for="sort-price">Price</label>
     </div>
 
@@ -31,9 +31,15 @@ const createTripSortTemplate = () => (
 );
 
 export default class TripSortView extends AbstractView {
+  #currentSortType = null;
+
+  constructor(currentSortType) {
+    super();
+    this.#currentSortType = currentSortType;
+  }
 
   get template() {
-    return createTripSortTemplate();
+    return createTripSortTemplate(/* this.#currentSortType */);
   }
 
   setSortTypeChangeHandler = (callback) => {
