dcc760c5b65fdee7a7cc6e444c8eaac9f1939961
capykasa <capykasa@gmail.com>
2022-09-24 13:25:20 +0500

    Добавил css loader

--- src/view/loading-view.js
+++ src/view/loading-view.js
@@ -1,9 +1,14 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
 const createNoTaskTemplate = () => (
-  `<p class="trip-events__msg">
-    Loading...
-  </p>`
+  `<div class="trip-events__msg">
+    <div class="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>`
 );
 
 export default class LoadingView extends AbstractView {
